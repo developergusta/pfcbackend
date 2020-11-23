a007b2800ee5348f13e3942105651a35f923e461
oliveguga <olive.guga@gmail.com>
2020-11-22 23:36:29 -0300

    get ticket

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -178,7 +178,7 @@ namespace Ticket2U.API.Repositories
 
         public async Task<IEnumerable<Ticket>> GetTicketsByUser(int id)
         {
-            IQueryable<Ticket> query = _context.Tickets.Where(x => x.UserId == id).Include(x => x.Event).Include(x => x.Event.Address).Include(x => x.Cashback);
+            IQueryable<Ticket> query = _context.Tickets.Where(x => x.UserId == id).Include(x => x.Cashback);
             return await query.ToListAsync();
         }
 
