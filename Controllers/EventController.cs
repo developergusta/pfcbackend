85b7d872058603e71607da5cb951518d4b84a203
oliveguga <olive.guga@gmail.com>
2020-11-25 10:35:00 -0300

    update event

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -262,7 +262,7 @@ namespace Ticket2U.API.Controllers
             try
             {
                 var evento = await _repository.GetEvent(EventId);
-                if(evento.DateStart > DateTime.UtcNow.AddHours(-3)){
+                if(evento.DateStart <= DateTime.UtcNow.AddHours(-3)){
                     return this.StatusCode(StatusCodes.Status500InternalServerError, $"Não é possível alterar o evento se já tiver acontecido (ou estiver acontecendo)");
                 }
 
